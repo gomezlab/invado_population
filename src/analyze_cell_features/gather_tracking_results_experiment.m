7b30cbec7655ecf7b23b9d1206fa186c64b03cb7
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-07-26 18:47:16 +0000

    gather_tracking_results_experiment.m: 	-new script to send off all the tracking results gathering
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invado_population@7547 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ src/analyze_cell_features/gather_tracking_results_experiment.m
@@ -0,0 +1,31 @@
+function gather_tracking_results_experiment(base_dir,varargin)
+tic;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+
+i_p.addRequired('base_dir',@(x)exist(x,'dir') == 7);
+i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
+
+i_p.parse(base_dir,varargin{:});
+
+addpath('matlab_scripts');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fields = dir(base_dir);
+fields = filter_to_time_series(fields);
+
+for i=1:length(fields)
+    try
+        gather_tracking_results(fullfile(base_dir,fields(i).name));
+        disp(['Done with ', fullfile(base_dir,fields(i).name)]);
+    catch %#ok<CTCH>
+        disp(['Problem with ', fullfile(base_dir,fields(i).name)]);
+    end
+end
+
+toc;
