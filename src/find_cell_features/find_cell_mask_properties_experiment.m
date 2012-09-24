b6ac3444f413aa2957f4307772ded83324968463
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-09-24 14:57:29 +0000

    find_cell_mask_properties_experiment.m: 	-removing gelatin_min_value option
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invado_population@7626 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/find_cell_mask_properties_experiment.m
+++ src/find_cell_features/find_cell_mask_properties_experiment.m
@@ -7,7 +7,6 @@ i_p = inputParser;
 
 i_p.addRequired('base_dir',@(x)exist(x,'dir') == 7);
 
-i_p.addParamValue('gelatin_min_value',382,@(x)isnumeric(x) && x > 0);
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
 i_p.parse(base_dir,varargin{:});
@@ -23,8 +22,7 @@ fields = filter_to_time_series(fields);
 
 for i=1:length(fields)
     try
-        find_cell_mask_properties(fullfile(base_dir,fields(i).name), ...
-            'gelatin_min_value',i_p.Results.gelatin_min_value);
+        find_cell_mask_properties(fullfile(base_dir,fields(i).name));
         disp(['Done with ', fullfile(base_dir,fields(i).name)]);
     catch %#ok<CTCH>
         disp(['Problem with ', fullfile(base_dir,fields(i).name)]);
