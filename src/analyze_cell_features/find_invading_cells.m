c2ed31acb01773333280794d80515cc3baf0107c
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-09-26 15:46:56 +0000

    find_invading_cells.m: 	-moving thresholds to program parameters
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/invado_population@7640 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/find_invading_cells.m
+++ src/analyze_cell_features/find_invading_cells.m
@@ -7,7 +7,10 @@ function find_invading_cells(field_dir,varargin)
 i_p = inputParser;
 
 i_p.addRequired('field_dir',@(x)exist(x,'dir') == 7);
-i_p.addParamValue('median_filter',-1.5,@isnumeric);
+
+i_p.addParamValue('total_degrade_threshold',-3,@(x)isnumeric(x));
+i_p.addParamValue('single_image_degrade_threshold',-0.7,@(x)isnumeric(x));
+
 i_p.addParamValue('debug',0,@(x)x == 1 || x == 0);
 
 i_p.parse(field_dir,varargin{:});
@@ -37,13 +40,8 @@ end
 
 data_series_folder = fullfile(field_dir,'cell_props','lin_time_series');
 
-files.p_vals = fullfile(data_series_folder,'Cell_gel_diff_p_val.csv');
-files.cell_diffs = fullfile(data_series_folder,'Cell_gel_diff.csv');
-files.cell_diff_medians = fullfile(data_series_folder,'Cell_gel_diff_median.csv');
-files.cell_diff_percents = fullfile(data_series_folder,'Cell_gel_diff_percent.csv');
 files.gel_diff_minus = fullfile(data_series_folder,'Gel_diff_minus_surrounding.csv');
 
-files.cell_total_degrade = fullfile(data_series_folder,'..','final_gel_diffs.csv');
 files.corrected_cell_total_degrade = fullfile(data_series_folder,'..','corrected_final_gel_diffs.csv');
 
 these_types = fieldnames(files);
@@ -64,8 +62,7 @@ for j = 1:length(these_types)
     end
 end
 
-raw_data.cell_total_degrade = repmat(raw_data.cell_total_degrade,1,size(raw_data.cell_diff_percents,2));
-raw_data.corrected_cell_total_degrade = repmat(raw_data.corrected_cell_total_degrade,1,size(raw_data.cell_diff_percents,2));
+raw_data.corrected_cell_total_degrade = repmat(raw_data.corrected_cell_total_degrade,1,size(raw_data.gel_diff_minus,2));
 
 %check that all the raw data files are the same size
 these_names = fieldnames(raw_data);
@@ -78,7 +75,8 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Process and Output 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-processed_data = process_raw_data(raw_data);
+processed_data = process_raw_data(raw_data,i_p.Results.total_degrade_threshold,...
+    i_p.Results.single_image_degrade_threshold);
 
 output_dir = fullfile(field_dir,'cell_props');
 
@@ -90,7 +88,8 @@ csvwrite(fullfile(output_dir,'longevity.csv'),processed_data.longevities);
 %% Functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function process_data = process_raw_data(raw_data,varargin)
+function process_data = process_raw_data(raw_data,total_degrade_threshold,...
+    single_image_degrade_threshold,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Setup variables and parse command line
@@ -99,9 +98,13 @@ function process_data = process_raw_data(raw_data,varargin)
 i_p = inputParser;
 
 i_p.addRequired('raw_data',@isstruct);
+
+i_p.addRequired('total_degrade_threshold',@(x)isnumeric(x));
+i_p.addRequired('single_image_degrade_threshold',@(x)isnumeric(x));
+
 i_p.addParamValue('filter_set',NaN,@islogical);
 
-i_p.parse(raw_data,varargin{:});
+i_p.parse(raw_data,total_degrade_threshold,single_image_degrade_threshold,varargin{:});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Main Program
@@ -117,11 +120,12 @@ end
 
 process_data = struct();
 
-process_data.active_degrade = not(isnan(raw_data.gel_diff_minus)) & raw_data.gel_diff_minus < -1.6 ...
-    & not(isnan(raw_data.corrected_cell_total_degrade)) & raw_data.corrected_cell_total_degrade < -0.4;
+process_data.active_degrade = not(isnan(raw_data.gel_diff_minus)) & ...
+    raw_data.gel_diff_minus < single_image_degrade_threshold & ...
+    not(isnan(raw_data.corrected_cell_total_degrade)) & ...
+    raw_data.corrected_cell_total_degrade < total_degrade_threshold;
 
 disp(['Detected ', num2str(sum(process_data.active_degrade(:))), ' invasion events.']);
-% disp(['Bonferroni Corrected p-value threshold: ', num2str(0.05/bonferroni_correction)]);
 
 process_data.live_cells = raw_data.tracking > 0;
 process_data.longevities = sum(process_data.live_cells,2)/2;
